d66040ab4c83c594951851b1b47113f4f5626c62
AZAZ59 <step-srg@yandex.ru>
2017-08-08 14:14:53 +0400

    fit doc2vec hyperparameters

--- src/main/java/starter/doc2vecTest.java
+++ src/main/java/starter/doc2vecTest.java
@@ -5,16 +5,22 @@ import cc.mallet.pipe.iterator.CsvIterator;
 import cc.mallet.types.InstanceList;
 import cc.mallet.types.Token;
 import cc.mallet.types.TokenSequence;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import org.deeplearning4j.text.sentenceiterator.labelaware.LabelAwareListSentenceIterator;
 import org.deeplearning4j.text.sentenceiterator.labelaware.LabelAwareSentenceIterator;
 import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
 import org.jetbrains.annotations.NotNull;
+import org.nd4j.linalg.api.buffer.DataBuffer;
+import org.nd4j.linalg.api.buffer.util.DataTypeUtil;
+import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.LoggerFactory;
 import pipes.FunctionToPipe;
 import pipes.TokenSequence2Stem;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.regex.Pattern;
 
 /**
@@ -22,58 +28,78 @@ import java.util.regex.Pattern;
  */
 public class doc2vecTest {
 
-    public static final boolean TRAIN = true;
+    public static boolean TRAIN = false;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        TRAIN=true;
+
 //        preprocessFile("./data/typed.csv","./data/stammed_clf.csv",true);
 //        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("./data/stammed_clf.csv")));
         ParagraphVectors build = null;
         if (TRAIN) {
             build = trainDoc2Vec();
         } else {
-            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("models_Doc2vec.bin")));
-            build = (ParagraphVectors) ois.readObject();
+            build=WordVectorSerializer.readParagraphVectors("models/Doc2vec.bin");
             build.setTokenizerFactory(new DefaultTokenizerFactory());
         }
 
+        testDoc2vec(build);
+
+    }
+
+    private static void testDoc2vec(ParagraphVectors build) throws IOException {
         LabelAwareSentenceIterator it = new LabelAwareListSentenceIterator(new FileInputStream("./data/test_clf.csv"), ":::::::", 1, 0);
         int all = 0;
         int trues = 0;
+        HashSet<String> used = new HashSet<>();
         while (it.hasNext()) {
             String sent = it.nextSentence();
             String currentLabel = it.currentLabels().get(0);
-            try {
-                String nearestLabel = build.nearestLabels(sent, 1).iterator().next();
-                if (currentLabel.equalsIgnoreCase(nearestLabel)) {
-                    trues++;
-                } else {
-                    System.out.println(sent + " " + currentLabel + " " + nearestLabel);
+            if(!used.contains(sent)) {
+                used.add(sent);
+                try {
+                    String nearestLabel = build.nearestLabels(sent, 1).iterator().next();
+                    if (currentLabel.equalsIgnoreCase(nearestLabel)) {
+                        trues++;
+                    } else {
+                        System.out.println(sent + " " + currentLabel + " " + nearestLabel);
+                    }
+                    all++;
+                } catch (Exception e) {
+//                e.printStackTrace();
+//                return;
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-            all++;
+
         }
         System.out.println("all:" + all);
         System.out.println("trues:" + trues);
         System.out.println("trues/all:" + ((trues + 0.0) / all));
-
     }
 
     @NotNull
     private static ParagraphVectors trainDoc2Vec() throws IOException {
         LabelAwareSentenceIterator it = new LabelAwareListSentenceIterator(new FileInputStream("./data/train_clf.csv"), ":::::::", 1, 0);
 
+//        DataTypeUtil.setDTypeForContext(DataBuffer.Type.HALF);
+//4190 Sequences/sec
+        /*
+        14:03:15.246 [VectorCalculationsThread 1] INFO  o.d.m.s.SequenceVectors - Epoch: [1]; Words vectorized so far: [954956];  Lines vectorized so far: [100000]; Seq/sec: [3550,13]; Words/sec: [33902,16]; learningRate: [0.006557565110986477]
+        */
         ParagraphVectors build = new ParagraphVectors.Builder()
+                .seed(1337)
                 .learningRate(0.025)
                 .minLearningRate(0.001)
                 .useHierarchicSoftmax(true)
                 .windowSize(10)
                 .layerSize(100)
+//                .minWordFrequency(10)
+                .useAdaGrad(true)  ///TODO
                 .trainElementsRepresentation(true)
                 .trainSequencesRepresentation(true)
                 .batchSize(100)
-                .epochs(75)
+                .epochs(20)
+                .iterations(3)
                 .iterate(it)
                 .workers(4)
                 .trainWordVectors(true)
@@ -81,9 +107,7 @@ public class doc2vecTest {
                 .build();
         build.fit();
 
-        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("models_Doc2vec.bin")));
-        oos.writeObject(build);
-        oos.close();
+        WordVectorSerializer.writeParagraphVectors(build,"models/Doc2vec_CUDA.bin");
         return build;
     }
 
